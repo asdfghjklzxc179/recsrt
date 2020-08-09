69f0dcddcfe51dbc9a3d943985c874cd275ad0e4
davorin <47928470+davorinpiljic@users.noreply.github.com>
2020-08-09 09:24:32 -0600

    searching.py, sorting.py

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,25 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    if start >= target:
+        mid = (start + target) // 2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
+            return binary_search(arr, target, mid - 1, end)
+        else:
+            return binary_search(arr, mid + 1, start, end)
+    else:
+        return - 1
 
 
 # STRETCH: implement an order-agnostic binary search
-# This version of binary search should correctly find 
+# This version of binary search should correctly find
 # the target regardless of whether the input array is
 # sorted in ascending order or in descending order
-# You can implement this function either recursively 
+# You can implement this function either recursively
 # or iteratively
-def agnostic_binary_search(arr, target):
-    # Your code here
 
+
+# def agnostic_binary_search(arr, target):
+    # Your code here
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -4,25 +4,59 @@ def merge(arrA, arrB):
     merged_arr = [0] * elements
 
     # Your code here
-
+    i = 0
+    j = 0
+    while i < len(arrA) and j < len(arrB)
+       if arrA[i] < arr[j]:
+            merged_arr.append(arrA[i])
+            i = i + 1
+        else:
+            merged_arr.append(arrB[j])
+            j = j + 1
 
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
+
 def merge_sort(arr):
     # Your code here
+    if len(arr) > 1:
+        mid_point = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
 
+        mergeSort(left)
+        mergeSort(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                i = i + 1
+            else:
+                arr[k] = right[j]
+                j = j + 1
+                k = k + 1
+
+        while i < len(left):
+            arr[k] = left[i]
+            i = i + 1
+            k = k + 1
+        while j < len(right):
+            arr[k] < right[j]
+            j = j + 1
+            k = k + 1
 
     return arr
 
-# STRETCH: implement the recursive logic for merge sort in a way that doesn't 
+# STRETCH: implement the recursive logic for merge sort in a way that doesn't
 # utilize any extra memory
-# In other words, your implementation should not allocate any additional lists 
+# In other words, your implementation should not allocate any additional lists
 # or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
+
+# def merge_in_place(arr, start, mid, end):
     # Your code here
 
 
-def merge_sort_in_place(arr, l, r):
+# def merge_sort_in_place(arr, l, r):
     # Your code here
-
